d9657e8e124d7c9b41db5e067f73cc4602b9dd35
PAVAN-PEDDOLLA <pavan-peddolla_tmna>
2024-12-01 11:40:22 +0530

    Web Socket.io SetUp Done

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -2,15 +2,27 @@ import { useSelector } from "react-redux";
 import ChatArea from "./components/chat";
 import Header from "./components/header";
 import Sidebar from "./components/sidebar";
+import { io } from "socket.io-client";
+import { useEffect } from "react";
 
 function Home() {
     const { selectedChat } = useSelector(state => state.userReducer);
+    const socket = io('http://localhost:5000');
+
+    useEffect(() => {
+        socket.emit('send-message-all', { text: 'HI From John' });
+        socket.on('send-message-by-server', data => {
+            console.log(data);
+        }
+        )
+    }, [])
+
     return (
         <div className="home-page">
             <Header />
             <div className="main-content">
                 <Sidebar />
-                { selectedChat && <ChatArea />}
+                {selectedChat && <ChatArea />}
             </div>
         </div>
     )
--- server/app.js
+++ server/app.js
@@ -7,11 +7,23 @@ const messageRouter = require('./controllers/messageController');
 
 
 //use auth controller routers
-app.use(express.json())
+app.use(express.json());
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST']
+    }
+});
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
 app.use('/api/chat', chatRouter);
 app.use('/api/message', messageRouter);
 
+io.on('connection', socket => {
+    socket.on('send-message-all', data => {
+        socket.emit('send-message-by-server', "message from server: " + data.text)
+    })
+});
 
-module.exports = app;
+module.exports = server;
